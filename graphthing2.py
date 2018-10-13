c2c38018ecb007a2dbc1400a6a520787673ca190
Owen Mason <owenmason353@gmail.com>
2018-10-13 19:37:33 -0400

    initial commit

---
+++ graphthing2.py
@@ -0,0 +1,32 @@
+num1 = 1
+numC = 1
+stepnumber = 0
+stepmax = 0
+stepplot = []
+numbers = []
+
+while (numC != 10000):
+  while (num1 != 1):
+    if (num1 % 2) == 0:
+      num1 = (num1 / 2)
+      stepnumber = stepnumber + 1
+    else:
+      num1 = (3 * num1) + 1
+      stepnumber = stepnumber + 1
+    if (stepnumber > stepmax):
+      stepmax = stepnumber
+  stepplot.append(stepnumber)
+  numbers.append(numC)
+  numC = numC + 1 
+  num1 = numC
+  stepnumber = 0
+
+import matplotlib.pyplot as plt
+x = numbers
+y = stepplot
+plt.scatter(x, y, label="circles", color="blue", marker=".", s=10)
+plt.xlabel('Integer Value')
+plt.ylabel('Number of Steps Taken to Converge')
+plt.title('A Graphical Representation of the Collatz Problem')
+plt.legend()
+plt.show()
